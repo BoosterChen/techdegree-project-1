da1b5f512a4cf47d8a59b64382b05b908585d30a
boooster <chenguangyu1987@126.com>
2018-06-15 23:35:52 +0800

    add quotes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,47 @@
+// quote array
+const quotes = [
+    {
+        quote:'Love is so short, forgetting is so long.',
+        source:'Pablo Neruda',
+        citation:'Selected Poems',
+        year: '1990'
+    },
+    {
+        quote:'The advantages found in history seem to be of three kinds, as it amuses the fancy, as it improves the understanding, and as it strengthens virtue.',
+        source:'David Hume',
+        citation:'The philosophical works of David Hume',
+        year: '1854'
+    },
+    {
+        quote:'Be a loner. That gives you time to wonder, to search for the truth. Have holy curiosity. Make your life worth living',
+        source:'Albert Einstein',
+        citation:'Einstein and the Poet',
+        year: '1983'
+    },
+    {
+        quote:'Your time is limited, so don\'t waste it living someone else\'s life. Don\'t be trapped by dogma - which is living with the results of other people\'s thinking. Don\'t let the noise of others\' opinions drown out your own inner voice. And most important, have the courage to follow your heart and intuition.',
+        source:'Steve Jobs',
+        citation:'Address at Stanford University',
+        year: '2005'
+    },
+    {
+        quote:'I\'m not going to change the way I look or the way I feel to conform to anything. I\'ve always been a freak. So I\'ve been a freak all my life and I have to live with that, you know. I\'m one of those people.',
+        source:'John Lennon',
+        citation:'Interview',
+        year: '1980'
+    }
+];
+
 // event listener to respond to "Show another quote" button clicks
+function printQuote() {
+
+}
+
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+
+
+
+
 
